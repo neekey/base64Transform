399526b83df6f1e44c1761139799a60c1632a14c
neekey <ni184775761@gmail.com>
2012-03-14 23:29:04 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,65 @@
+
+/*
+ * GET home page.
+ */
+var Base64 = require( '../mods/base64.js' );
+var FS = require( 'fs' );
+
+module.exports = {
+
+    index: function( req, res ){
+        res.render('index', { title: 'Express' });
+    },
+    decode: function ( req, res ){
+
+
+        var path = 'tempfile/';
+        var tempName = path + Date.now() + Math.random();
+        var base64String = req.body.base64String;
+        var filename = req.body.filename || 'temp';
+
+        Base64.decode( base64String, tempName, function( err ){
+
+            if( err ){
+                res.send( JSON.stringify( err ) );
+            }
+            else {
+                res.download( tempName, filename, function( err ){
+                    if( err ){
+                        res.send( JSON.stringify( err ) );
+                    }
+                    else {
+                        console.log( 'file send success!' );
+                    }
+                });
+            }
+        });
+    },
+
+    encode: function( req, res ){
+
+        var fileObj = req.files.file;
+        console.log( fileObj );
+        console.log( fileObj.length );
+        console.log( fileObj.filename );
+        console.log( fileObj.mine );
+
+        Base64.encode( fileObj.path, function( err, base64String ){
+
+            if( err ){
+                res.send( JSON.stringify(err) );
+            }
+            else {
+                res.render( 'encode-result', { layout: false,base64String: base64String } );
+
+                // 删除文件
+                FS.unlink( fileObj.path, function( err ){
+
+                    if( err ){
+                        console.log( err );
+                    }
+                });
+            }
+        });
+    }
+};
